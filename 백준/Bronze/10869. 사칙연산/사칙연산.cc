a4cc2bdb08087dd3dabad76cb998abc86953c2a4
Seoeun Park <53970650+sseonxx@users.noreply.github.com>
2025-02-06 23:38:38 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+//문제번호:10869번
+int main()
+{
+	int a, b;
+
+	cin >> a;
+	cin >> b;
+
+	cout << a+b << endl;
+	cout << a-b << endl;
+	cout << a*b << endl;
+	cout << a/b << endl;
+	cout << a%b << endl;
+
+}
