121c6651905fa55f0fcb81747e604eb17ec69bb3
Seoeun Park <53970650+sseonxx@users.noreply.github.com>
2025-02-06 23:35:12 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int num, sum = 0, remainderArr[42] = { 0 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> num;
+		if(!remainderArr[num%42]++)
+			sum++;
+	}
+	cout << sum<<'\n';
+}
