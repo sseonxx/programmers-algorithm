1fa2d7ca5a0cb421ba2e039f77c9bd84c99ec38f
Seoeun Park <53970650+sseonxx@users.noreply.github.com>
2025-02-06 23:39:13 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+//문제:10950
+
+int main()
+{
+	int num, a, b,i;
+	cin >> num;
+
+	for (i = 1; i <= num; i++)
+	{
+		cin >> a >> b;
+
+		cout << a + b << endl;
+	}
+
+}
