d593af739ad8421763ed6d56554f91dbd34e2eae
Seoeun Park <53970650+sseonxx@users.noreply.github.com>
2025-02-06 23:40:10 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int num,a,b;
+	cin >> num;
+
+	for (int i = 1; i<=num; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a+b << '\n';
+	}
+
+	return 0;
+
+}
