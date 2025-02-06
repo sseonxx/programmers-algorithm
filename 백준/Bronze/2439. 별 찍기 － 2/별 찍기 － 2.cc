7c3a26aeb5df669e0e293bf46d43f03e18b549ce
Seoeun Park <53970650+sseonxx@users.noreply.github.com>
2025-02-06 23:31:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int num;
+	cin >> num;
+
+	for (int i = 1; i <= num; i++)
+	{
+		for (int x =num-i;x>0;x-- )
+		{
+			cout << " ";
+		}
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+
+}
